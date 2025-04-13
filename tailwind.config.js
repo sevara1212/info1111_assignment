fb542f526fddab7c9623944d107ac5ee220a397e
sevara1212 <ibragimovasevara10@gmail.com>
2025-04-13 22:57:24 +1000

    DDD

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      './app/**/*.{js,ts,jsx,tsx}',
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
+      './pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './components/**/*.{js,ts,jsx,tsx,mdx}',
+      './app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          background: 'rgb(var(--background-rgb))',
+          foreground: 'rgb(var(--foreground-rgb))',
+        },
+      },
     },
     plugins: [],
+    future: {
+      removeDeprecatedGapUtilities: true,
+      purgeLayersByDefault: true,
+    },
   };
   
